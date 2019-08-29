4c0e7fda430afcefd7834e14ec666412a20fe47f
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2019-08-29 13:32:41 +0900

    Fix Indent

--- contracts/DataTypes.sol
+++ contracts/DataTypes.sol
@@ -2,53 +2,53 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract DataTypes {
-  struct Property {
-    address predicate;
-    bytes input;
-  }
+    struct Property {
+        address predicate;
+        bytes input;
+    }
 
-  struct Contradiction {
-    Property property;
-    Property counterProperty;
-  }
+    struct Contradiction {
+        Property property;
+        Property counterProperty;
+    }
 
-  struct ClaimStatus {
-    Property property;
-    uint numProvenContradictions;
-    uint decidedAfter; // block number where the claims are decided  
-  }
+    struct ClaimStatus {
+        Property property;
+        uint numProvenContradictions;
+        uint decidedAfter; // block number where the claims are decided  
+    }
 
-  struct ImplicationProofElement {
-    Property implication;
-    bytes[] witness;
-  }
+    struct ImplicationProofElement {
+        Property implication;
+        bytes[] witness;
+    }
 
-  struct CheckpointStatus {
-    uint256 challengeableUntil;
-    uint256 outstandingChallenges;
-  }
+    struct CheckpointStatus {
+        uint256 challengeableUntil;
+        uint256 outstandingChallenges;
+    }
 
-  struct Challenge {
-    types.Checkpoint challengedCheckpoint;
-    types.Checkpoint challengingCheckpoint;
-  } 
-  struct StateObject {
-    address predicateAddress;
-    bytes data;
-  }
+    struct Challenge {
+        types.Checkpoint challengedCheckpoint;
+        types.Checkpoint challengingCheckpoint;
+    } 
+    struct StateObject {
+        address predicateAddress;
+        bytes data;
+    }
 
-  struct Range {
-    uint256 start;
-    uint256 end;
-  }
-  struct StateUpdate {
-    StateObject stateObject;
-    Range range;
-    uint256 plasmaBlockNumber;
-    address depositAddress;
-  }
-  struct Checkpoint {
-    StateUpdate stateUpdate;
-    Range subrange;
+    struct Range {
+        uint256 start;
+        uint256 end;
+    }
+    struct StateUpdate {
+        StateObject stateObject;
+        Range range;
+        uint256 plasmaBlockNumber;
+        address depositAddress;
+    }
+    struct Checkpoint {
+        StateUpdate stateUpdate;
+        Range subrange;
     }
 }
