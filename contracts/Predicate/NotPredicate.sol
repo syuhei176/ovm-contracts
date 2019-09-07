bbfc126e13a7b22205f470383f32dc60678a5c10
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2019-09-06 21:30:40 -0400

    Fix DataTypes

--- contracts/Predicate/NotPredicate.sol
+++ contracts/Predicate/NotPredicate.sol
@@ -1,30 +1,35 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-contract DataTypes {
-    struct Property {
-        address predicateAddress;
-        bytes input;
-    }
-
-    struct NOTContradictionWitness {
-        uint NOTIndex;
-    }
-}
-
+import {DataTypes as types} from "../DataTypes.sol";
 
 // Predicate returning true if a given proeprty is false
 contract NotPredicate {
-    function verifyContradiction(DataTypes.Property memory _claim0, DataTypes.Property memory _claim1, DataTypes.NOTContradictionWitness memory _contradictionWitness) public returns (bool) {
+    struct NOTContradictionWitness {
+        uint NOTIndex;
+    }
+    struct NotPredicateInput {
+        types.Property property;
+    }
+
+    function verifyContradiction(
+        types.Property memory _claim0,
+        types.Property memory _claim1,
+        NOTContradictionWitness memory _contradictionWitness
+    ) public returns (bool) {
         //a Not property contradicts to a base property if notProperty.input == baseProperty
-        DataTypes.Property[2] memory claims = [_claim0, _claim1];
-        require(_contradictionWitness.NOTIndex == 0 || _contradictionWitness.NOTIndex == 1);
-        DataTypes.Property memory baseClaim = claims[_contradictionWitness.NOTIndex];
-        DataTypes.Property memory NOTBaseClaim = claims[(_contradictionWitness.NOTIndex - 1)**2];
+        types.Property[2] memory claims = [_claim0, _claim1];
+        require(_contradictionWitness.NOTIndex == 0 || _contradictionWitness.NOTIndex == 1,
+        "contradictionWitness's Index should be either 0 or 1");
+        types.Property memory baseClaim = claims[_contradictionWitness.NOTIndex];
+        types.Property memory NOTBaseClaim = claims[(_contradictionWitness.NOTIndex - 1)**2];
         return ((NOTBaseClaim.predicateAddress == address(this)) && (keccak256(NOTBaseClaim.input) == keccak256(abi.encode(baseClaim))));
     }
 
-    function verifyImplication(DataTypes.Property memory _thisClaim, DataTypes.Property memory _implication) public returns (bool) {
+    function verifyImplication(
+        types.Property memory _thisClaim,
+        types.Property memory _implication
+    ) public returns (bool) {
         //NOT(NOT(P)) implies P
         if ((_thisClaim.predicateAddress == address(this)) && (keccak256(_thisClaim.input) == keccak256(abi.encode(_implication)))
         ) {
