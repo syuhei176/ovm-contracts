da03379681b7fce4650963cc1fe5e07bff691fe3
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2019-09-06 22:08:00 -0400

    Fix Property to Input

--- contracts/Predicate/NotPredicate.sol
+++ contracts/Predicate/NotPredicate.sol
@@ -8,30 +8,30 @@ contract NotPredicate {
     struct NOTContradictionWitness {
         uint NOTIndex;
     }
-    struct NotPredicateInput {
+    struct NOTPredicateInput {
         types.Property property;
     }
 
     function verifyContradiction(
-        types.Property memory _claim0,
-        types.Property memory _claim1,
+        NOTPredicateInput memory _claim0,
+        NOTPredicateInput memory _claim1,
         NOTContradictionWitness memory _contradictionWitness
     ) public returns (bool) {
         //a Not property contradicts to a base property if notProperty.input == baseProperty
-        types.Property[2] memory claims = [_claim0, _claim1];
+        NOTPredicateInput[2] memory claims = [_claim0, _claim1];
         require(_contradictionWitness.NOTIndex == 0 || _contradictionWitness.NOTIndex == 1,
         "contradictionWitness's Index should be either 0 or 1");
-        types.Property memory baseClaim = claims[_contradictionWitness.NOTIndex];
-        types.Property memory NOTBaseClaim = claims[(_contradictionWitness.NOTIndex - 1)**2];
-        return ((NOTBaseClaim.predicateAddress == address(this)) && (keccak256(NOTBaseClaim.input) == keccak256(abi.encode(baseClaim))));
+        NOTPredicateInput memory baseClaim = claims[_contradictionWitness.NOTIndex];
+        NOTPredicateInput memory NOTBaseClaim = claims[(_contradictionWitness.NOTIndex - 1)**2];
+        return ((NOTBaseClaim.property.predicateAddress == address(this)) && (keccak256(NOTBaseClaim.property.input) == keccak256(abi.encode(baseClaim))));
     }
 
     function verifyImplication(
-        types.Property memory _thisClaim,
-        types.Property memory _implication
+        NOTPredicateInput memory _thisClaim,
+        NOTPredicateInput memory _implication
     ) public returns (bool) {
         //NOT(NOT(P)) implies P
-        if ((_thisClaim.predicateAddress == address(this)) && (keccak256(_thisClaim.input) == keccak256(abi.encode(_implication)))
+        if ((_thisClaim.property.predicateAddress == address(this)) && (keccak256(_thisClaim.property.input) == keccak256(abi.encode(_implication)))
         ) {
             return true;
         } else {
