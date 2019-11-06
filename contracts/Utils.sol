7d23c1c13797e649f62bdb965162fcaa6049c681
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-06 16:58:21 +0900

    Remove isEmptyClaim from Utils

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -7,8 +7,4 @@ contract Utils {
     function getPropertyId(types.Property memory _property) public pure returns (bytes32) {
         return keccak256(abi.encode(_property));
     }
-
-    function isEmptyClaim(types.ChallengeGame memory _game) internal pure returns (bool) {
-        return _game.createdBlock == 0;
-    }
 }
