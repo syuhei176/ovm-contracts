8876e6932597e12eaef265edce146b3c24f4c595
Shuhei Hiya <syuhei176@gmail.com>
2019-11-07 10:41:42 +0900

    specify name

--- contracts/Predicate/LogicalConnective.sol
+++ contracts/Predicate/LogicalConnective.sol
@@ -4,5 +4,5 @@ pragma experimental ABIEncoderV2;
 import {DataTypes as types} from "../DataTypes.sol";
 
 interface LogicalConnective {
-    function isValidChallenge(bytes[] calldata inputs, bytes calldata, types.Property calldata) external returns (bool);
+    function isValidChallenge(bytes[] calldata _inputs, bytes calldata _challengeInput, types.Property calldata _challenge) external returns (bool);
 }
