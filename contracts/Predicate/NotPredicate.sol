03c55b38584da4f2eb1651ed78d189df15fef5b0
Shuhei Hiya <syuhei176@gmail.com>
2019-11-07 10:40:07 +0900

    Fix typo

--- contracts/Predicate/NotPredicate.sol
+++ contracts/Predicate/NotPredicate.sol
@@ -29,7 +29,7 @@ contract NotPredicate is LogicalConnective {
     /**
      * @dev Validates a child node of Not property in game tree.
      */
-    function isValidChallenge(bytes[] calldata _inputs, bytes calldata _challengeInput, types.Property calldata _challnge) external returns (bool) {
+    function isValidChallenge(bytes[] calldata _inputs, bytes calldata _challengeInput, types.Property calldata _challenge) external returns (bool) {
         // The valid challenge of not(p) is p and _inputs[0] is p here
         return keccak256(_inputs[0]) == keccak256(abi.encode(_challnge));
     }
