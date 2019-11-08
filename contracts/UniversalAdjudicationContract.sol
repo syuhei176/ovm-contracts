364af4ba89a4412268341bb8ef8e2ec8a92edac2
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-08 14:01:23 +0900

    Clean attributes names

--- contracts/UniversalAdjudicationContract.sol
+++ contracts/UniversalAdjudicationContract.sol
@@ -10,7 +10,7 @@ import "./Predicate/LogicalConnective.sol";
 contract UniversalAdjudicationContract {
 
     uint DISPUTE_PERIOD = 7;
-    mapping (bytes32 => types.ChallengeGame) public claims;
+    mapping (bytes32 => types.ChallengeGame) public instantiatedGames;
     mapping (bytes32 => bool) contradictions;
     enum RemainingClaimIndex {Property, CounterProperty}
     Utils utils;
@@ -19,24 +19,27 @@ contract UniversalAdjudicationContract {
         utils = Utils(_utilsAddress);
     }
 
+    /**
+     * @dev Claims property and create new game. Id of game is hash of claimed property
+     */
     function claimProperty(types.Property memory _claim) public {
         // get the id of this property
-        bytes32 claimedPropertyId = utils.getPropertyId(_claim);
+        bytes32 gameId = utils.getPropertyId(_claim);
         // make sure a claim on this property has not already been made
-        require(isEmptyClaim(claims[claimedPropertyId]), "claim isn't empty");
+        require(isEmptyClaim(instantiatedGames[gameId]), "claim isn't empty");
 
         // create the claim status. Always begins with no proven contradictions
         types.ChallengeGame memory newGame = types.ChallengeGame(_claim, new bytes32[](0), types.Decision.Undecided, block.number);
 
         // store the claim
-        claims[claimedPropertyId] = newGame;
+        instantiatedGames[gameId] = newGame;
     }
 
     function decideClaimToTrue(bytes32 _gameId) public {
-        types.ChallengeGame storage game = claims[_gameId];
+        types.ChallengeGame storage game = instantiatedGames[_gameId];
         // check all _game.challenges should be false
         for (uint256 i = 0;i < game.challenges.length; i++) {
-            types.ChallengeGame memory challengingGame = claims[game.challenges[i]];
+            types.ChallengeGame memory challengingGame = instantiatedGames[game.challenges[i]];
             require(challengingGame.decision == types.Decision.False, "all _game.challenges should be false");
         }
         // should check dispute period
@@ -49,8 +52,8 @@ contract UniversalAdjudicationContract {
         bytes32 _gameId,
         bytes32 _challengingGameId
     ) public {
-        types.ChallengeGame storage game = claims[_gameId];
-        types.ChallengeGame memory challengingGame = claims[_challengingGameId];
+        types.ChallengeGame storage game = instantiatedGames[_gameId];
+        types.ChallengeGame memory challengingGame = instantiatedGames[_challengingGameId];
         // check _challenge is in _game.challenges
         bytes32 challengingGameId = utils.getPropertyId(challengingGame.property);
         bool isValidChallenge = false;
@@ -80,19 +83,22 @@ contract UniversalAdjudicationContract {
 
         // if the decision is true, automatically decide its claim now
         if (_decision) {
-            claims[decidedPropertyId].decision = types.Decision.True; // True
+            instantiatedGames[decidedPropertyId].decision = types.Decision.True; // True
         } else {
-            claims[decidedPropertyId].decision = types.Decision.False; // False
+            instantiatedGames[decidedPropertyId].decision = types.Decision.False; // False
         }
     }
 
+    /**
+     * @dev challenge a game specified by gameId with a challengingGame specified by _challengingGameId
+     */
     function challenge(
-        bytes32 gameId,
+        bytes32 _gameId,
         bytes[] memory _challengeInputs,
         bytes32 _challengingGameId
     ) public returns (bool) {
-        types.ChallengeGame storage game = claims[gameId];
-        types.ChallengeGame memory challengingGame = claims[_challengingGameId];
+        types.ChallengeGame storage game = instantiatedGames[_gameId];
+        types.ChallengeGame memory challengingGame = instantiatedGames[_challengingGameId];
         require(
             LogicalConnective(game.property.predicateAddress).isValidChallenge(game.property.inputs, _challengeInputs[0], challengingGame.property),
             "_challenge isn't valid"
@@ -107,11 +113,11 @@ contract UniversalAdjudicationContract {
     }
 
     function isDecided(types.Property memory _property) public returns (bool) {
-        return claims[utils.getPropertyId(_property)].decision == types.Decision.True;
+        return instantiatedGames[utils.getPropertyId(_property)].decision == types.Decision.True;
     }
 
     function getGame(bytes32 claimId) public view returns (types.ChallengeGame memory) {
-        return claims[claimId];
+        return instantiatedGames[claimId];
     }
 
     function getPropertyId(types.Property memory _property) public view returns (bytes32) {
