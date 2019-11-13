a8853a35aaa01b899d2658f74b5f4f69c1dd8a81
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-13 19:42:28 +0900

    Use typescript for test

--- test/helpers/getGameId.js
+++ test/helpers/getGameId.js
@@ -1,7 +1,7 @@
 const utils = require('ethers/utils');
 const abi = new utils.AbiCoder();
 
-function getGameIdFromProperty(property) {
+function getGameIdFromProperty(property: ) {
   return utils.keccak256(abi.encode(['tuple(address, bytes[])'], [[property.predicateAddress, property.inputs]]));
 }
 
