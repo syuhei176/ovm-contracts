4c90dc8b9499d54dc46812c3bd7d80ea8d4afa23
SHUHEI HIYA <syuhei176@gmail.com>
2019-11-21 19:23:42 +0900

    test(predicate): Clean test code for IsValidSignaturePredicate #49

--- test/predicate/atomic/IsValidSignaturePredicate.test.ts
+++ test/predicate/atomic/IsValidSignaturePredicate.test.ts
@@ -9,6 +9,7 @@ import * as MockAdjudicationContract from '../../../build/MockAdjudicationContra
 import * as Utils from '../../../build/Utils.json'
 import * as IsValidSignaturePredicate from '../../../build/IsValidSignaturePredicate.json'
 import * as ethers from 'ethers'
+import { hexlify, toUtf8Bytes } from 'ethers/utils'
 
 chai.use(solidity)
 chai.use(require('chai-as-promised'))
@@ -38,25 +39,39 @@ describe('IsValidSignaturePredicate', () => {
 
   describe('decideTrue', () => {
     const verifierType = ethers.utils.toUtf8Bytes('secp256k1')
+    const address = '0xa7E678F5F3Db99bf4957AC2ebEb3a89C6f9031F6'
+    const signature =
+      '0x3050ed8255d5599ebce4be5ef23eceeb61bfae924db5e5b12fc975663854629204a68351940fcea4231e9e4af515e2a10c187fcd7f88f4e5ffed218c76a5553b1b'
+    const invalidSignature =
+      '0x00b0ed8255d5599ebce4be5ef23eceeb16bfae924db5e5b12fc975663854629204a68351940fcea4231e9e4af515e2a10c187fcd7f88f4e5ffed218c76a1113bb2'
+
+    const message = hexlify(toUtf8Bytes('message'))
     it('suceed to decide', async () => {
-      const wallet = ethers.Wallet.createRandom()
-      const signingKey = new ethers.utils.SigningKey(wallet.privateKey)
-      const message = ethers.utils.toUtf8Bytes('message')
-      const signature = ethers.utils.joinSignature(
-        signingKey.signDigest(
-          ethers.utils.arrayify(ethers.utils.keccak256(message))
-        )
-      )
       await isValidSignaturePredicate.decideTrue([
-        ethers.utils.hexlify(message),
+        message,
         signature,
-        wallet.address,
+        address,
         verifierType
       ])
     })
-    it('fail to decide', async () => {
+    it('fail to decide with invalid signature', async () => {
       await expect(
-        isValidSignaturePredicate.decideTrue(['0x01', '0x', '0x', verifierType])
+        isValidSignaturePredicate.decideTrue([
+          message,
+          invalidSignature,
+          address,
+          verifierType
+        ])
+      ).to.be.reverted
+    })
+    it('fail to decide with empty signature', async () => {
+      await expect(
+        isValidSignaturePredicate.decideTrue([
+          message,
+          '0x',
+          address,
+          verifierType
+        ])
       ).to.be.reverted
     })
   })
