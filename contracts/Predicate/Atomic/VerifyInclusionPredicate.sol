a7218e75bf80982a7d73438e318b1a0d6f9248be
SHUHEI HIYA <syuhei176@gmail.com>
2019-12-06 22:03:34 +0900

    feat: add VerifyInclusionPredicate #50

---
+++ contracts/Predicate/Atomic/VerifyInclusionPredicate.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import { DataTypes as types } from "../../DataTypes.sol";
+import "../AtomicPredicate.sol";
+import { UniversalAdjudicationContract } from "../../UniversalAdjudicationContract.sol";
+import "../../Utils.sol";
+import "../../CommitmentContract.sol";
+
+contract VerifyInclusionPredicate is AtomicPredicate {
+    address uacAddress;
+    Utils utils;
+    CommitmentContract commitmentContract;
+
+    constructor(address _uacAddress, address _utilsAddress, address _commitmentContract) public {
+        uacAddress = _uacAddress;
+        utils = Utils(_utilsAddress);
+        commitmentContract = CommitmentContract(_commitmentContract);
+    }
+
+    function decide(bytes[] memory _inputs) public view returns(bool) {
+        return commitmentContract.verifyInclusion(
+            keccak256(_inputs[0]),
+            utils.bytesToAddress(_inputs[1]),
+            abi.decode(_inputs[2], (types.Range)),
+            abi.decode(_inputs[3], (types.InclusionProof)),
+            abi.decode(_inputs[4], (uint256))
+        );
+    }
+
+    function decideTrue(bytes[] memory _inputs) public {
+        require(decide(_inputs), "must decide true");
+        types.Property memory property = types.Property({
+            predicateAddress: address(this),
+            inputs: _inputs
+        });
+        UniversalAdjudicationContract(uacAddress).setPredicateDecision(utils.getPropertyId(property), true);
+    }
+}
--- contracts/test/MockCommitmentContract.sol
+++ contracts/test/MockCommitmentContract.sol
@@ -1,10 +1,21 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+import { DataTypes as types } from "../DataTypes.sol";
+
 /**
  * @title MockCommitmentContract
  * @notice This is mock commitment contract
  */
 contract MockCommitmentContract{
     uint256 public currentBlock = 100;
+    function verifyInclusion(
+        bytes32 _leaf,
+        address _tokenAddress,
+        types.Range memory _range,
+        types.InclusionProof memory _inclusionProof,
+        uint256 _blkNumber
+    ) public pure returns (bool) {
+        return true;
+    }
 }
