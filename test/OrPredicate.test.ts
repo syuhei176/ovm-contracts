1dcc9142307f1d026f0c8cc2c9e0006bbae1d8b8
Shuhei Hiya <syuhei176@gmail.com>
2020-01-26 10:42:08 +0900

    feat: add OrPredicate

---
+++ test/OrPredicate.test.ts
@@ -0,0 +1,136 @@
+/* contract imports */
+import chai from 'chai'
+import {
+  createMockProvider,
+  deployContract,
+  getWallets,
+  solidity
+} from 'ethereum-waffle'
+import * as MockChallenge from '../build/contracts/MockChallenge.json'
+import * as OrPredicate from '../build/contracts/OrPredicate.json'
+import * as ethers from 'ethers'
+const abi = new ethers.utils.AbiCoder()
+import { OvmProperty, randomAddress, encodeProperty } from './helpers/utils'
+
+chai.use(solidity)
+chai.use(require('chai-as-promised'))
+const { expect, assert } = chai
+
+describe('OrPredicate', () => {
+  let provider = createMockProvider()
+  let wallets = getWallets(provider)
+  let wallet = wallets[0]
+  let orPredicate: ethers.Contract
+  const boolAddress = randomAddress()
+  const notAddress = randomAddress()
+  const andAddress = randomAddress()
+  let mockChallenge: ethers.Contract
+  let orProperty: OvmProperty
+
+  beforeEach(async () => {
+    mockChallenge = await deployContract(wallet, MockChallenge, [])
+    orPredicate = await deployContract(wallet, OrPredicate, [
+      notAddress,
+      andAddress
+    ])
+    orProperty = {
+      predicateAddress: orPredicate.address,
+      inputs: [
+        encodeProperty({
+          predicateAddress: boolAddress,
+          inputs: ['0x01']
+        }),
+        encodeProperty({
+          predicateAddress: boolAddress,
+          inputs: ['0x02']
+        })
+      ]
+    }
+  })
+
+  describe('isValidChallenge', () => {
+    it('suceed to challenge or(a, b) with and(not(a), not(b))', async () => {
+      const challengeProperty = {
+        predicateAddress: andAddress,
+        inputs: [
+          encodeProperty({
+            predicateAddress: notAddress,
+            inputs: [
+              encodeProperty({
+                predicateAddress: boolAddress,
+                inputs: ['0x01']
+              })
+            ]
+          }),
+          encodeProperty({
+            predicateAddress: notAddress,
+            inputs: [
+              encodeProperty({
+                predicateAddress: boolAddress,
+                inputs: ['0x02']
+              })
+            ]
+          })
+        ]
+      }
+      const result = await mockChallenge.isValidChallenge(
+        orProperty,
+        [],
+        challengeProperty
+      )
+      assert.isTrue(result)
+    })
+    it('fail to challenge or(a, b) with and(not(a), b)', async () => {
+      const challengeProperty = {
+        predicateAddress: andAddress,
+        inputs: [
+          encodeProperty({
+            predicateAddress: notAddress,
+            inputs: [
+              encodeProperty({
+                predicateAddress: boolAddress,
+                inputs: ['0x01']
+              })
+            ]
+          }),
+          encodeProperty({
+            predicateAddress: boolAddress,
+            inputs: ['0x02']
+          })
+        ]
+      }
+      await expect(
+        mockChallenge.isValidChallenge(orProperty, [], challengeProperty)
+      ).to.be.revertedWith('inputs must be same')
+    })
+    it('fail to challenge or(a, b) with or(not(a), not(b))', async () => {
+      const challengeProperty = {
+        predicateAddress: orPredicate.address,
+        inputs: [
+          encodeProperty({
+            predicateAddress: notAddress,
+            inputs: [
+              encodeProperty({
+                predicateAddress: boolAddress,
+                inputs: ['0x01']
+              })
+            ]
+          }),
+          encodeProperty({
+            predicateAddress: notAddress,
+            inputs: [
+              encodeProperty({
+                predicateAddress: boolAddress,
+                inputs: ['0x02']
+              })
+            ]
+          })
+        ]
+      }
+
+      await expect(
+        mockChallenge.isValidChallenge(orProperty, [], challengeProperty)
+      ).to.be.revertedWith('challenge must be And')
+    })
+  })
+})
